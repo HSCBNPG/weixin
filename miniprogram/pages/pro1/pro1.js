7b355c674bf32bef0cb5096100c04b9437dcbc42
HSCBNPG <1903715601@qq.com>
2019-10-12 18:18:28 +0800

    取个名字吧

--- miniprogram/pages/pro1/pro1.js
+++ miniprogram/pages/pro1/pro1.js
@@ -5,14 +5,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    indicatorDots:true,
+    autoplay:true,
+    imagesList:[
+      { url:"../../images/code-cloud-callback-config.png"},
+      { url:"../../images/code-db-inc-dec.png"}
+    ],  //轮播图图片
+    abc:0,
+    num:9
   },
-
+  // 轮播一次后出发的事件
+  changeS(event){
+    // 当前滑块的index,itemid,事件触发原因
+    console.log(event.detail)
+  },
+  // 轮播图滚动中触发
+  changeT(event){
+    // left，top值
+    // console.log(event.detail)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // setInterval(()=>{
+    //   console.log(this.data.abc)
+    // },1000)
   },
 
   /**
--- miniprogram/pages/pro3/pro3.js
+++ miniprogram/pages/pro3/pro3.js
@@ -1,20 +1,57 @@
 // pages/pro3/pro3.js
+const audio = wx.getBackgroundAudioManager();
+const db = wx.cloud.database({
+  env: "weixinapp-piafg"
+});
+const media = require("jsmediatags")
 Page({
-
+  
   /**
    * 页面的初始数据
    */
   data: {
-
+    userImage:"",   //用户头像
+    userName : "",  //用户名称
+    songs: [],      //歌单列表--其中包含歌曲url
+    songUrl: "",    //歌曲路径
+    songTitle:"",   //歌曲标题
+    sing:''         //歌手名称
   },
-
+  creat(e){
+    audio.title = '此时此刻'
+    audio.src = e.target.dataset.url;
+    console.log(this)    //全局
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 获取用户头像
+    var image = ""
+    new Promise(function(door){
+     wx.getUserInfo({
+       success(res){
+        image = res.userInfo.avatarUrl
+        door()
+       }
+     })
+    }).then(()=>{
+      this.setData({
+        userImage:image
+      })
+      console.log(this.data.userImage)
+    })
+    // 获取歌单列表
+    db.collection("song")
+    .get()
+    .then(res=>{
+      this.setData({
+        songs:res.data
+      })
+      console.log(this.data.songs)
+    })
   },
-
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
